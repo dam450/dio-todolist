3b3f8e780915ea2d5037a3f2c4bdac42ee9dacce
Dam450 <68165752+dam450@users.noreply.github.com>
2022-02-12 17:24:01 -0300

    task done function added
    adicionada função que quando selecionada checkbox da tarefa, ela é riscada.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,20 @@ const taskButton = document.getElementById('addtask')
 taskButton.addEventListener('click', addTask)
 
 function addTask() {
-  let item = task.value
-  if (item != '') {
-    task.value = ''
+  let itemHtml = `<input name="task" type="checkbox" onchange="taskDone(this)" /><span>${task.value}</span>`
+
+  if (task.value != '') {
     let node = document.createElement('li')
-    node.innerHTML = `<input type="checkbox" />${item}`
+    node.innerHTML = itemHtml
     taskList.appendChild(node)
+    task.value = ''
+  }
+}
+
+function taskDone(e) {
+  if (e.checked === true) {
+    e.parentElement.classList.add('done')
+  } else {
+    e.parentElement.classList.remove('done')
   }
 }
